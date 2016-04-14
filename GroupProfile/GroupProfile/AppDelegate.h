c469ef425b7295bde081054de85a1e14287b92ba
GodLikeEcho <victosme@gmail.com>
2016-04-14 13:07:31 -0700

    Project 2
    started a project separate from SP

---
+++ GroupProfile/GroupProfile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GroupProfile
+//
+//  Created by Clint Jellesed on 4/14/16.
+//  Copyright © 2016 Clint Jellesed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GroupProfile/GroupProfile/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  GroupProfile
+//
+//  Created by Clint Jellesed on 4/14/16.
+//  Copyright © 2016 Clint Jellesed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
