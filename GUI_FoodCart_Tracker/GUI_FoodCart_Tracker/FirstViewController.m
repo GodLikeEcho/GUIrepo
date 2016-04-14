085bd24e06ab1366d6a53cf243d0f97eb91191da
GodLikeEcho <victosme@gmail.com>
2016-04-13 20:20:29 -0700

    HomePage
    added list and clock/label

--- GUI_FoodCart_Tracker/GUI_FoodCart_Tracker/FirstViewController.m
+++ GUI_FoodCart_Tracker/GUI_FoodCart_Tracker/FirstViewController.m
@@ -9,6 +9,7 @@
 #import "FirstViewController.h"
 
 @interface FirstViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *Clock;
 
 @end
 
@@ -17,7 +18,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //[self updateTime];
+   }
+-(void)updateTime
+{
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc]init];
+    [dateFormat setDateFormat:@"hh-mm-ss"];
+    _Clock.text = [dateFormat stringFromDate:[NSDate date]];
+    
+    
+    //call update every second
+    [self performSelector:@selector(updateTime) withObject:self afterDelay:1.0];
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
