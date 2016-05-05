0306a706324bdda5a1443e1ef0006f044aacefb0
GodLikeEcho <victosme@gmail.com>
2016-05-04 23:58:55 -0700

    Music and UI
    Updated UI and added music with a play and pause controler.

--- GroupProfile2/GroupProfile2/FirstViewController.swift
+++ GroupProfile2/GroupProfile2/FirstViewController.swift
@@ -7,9 +7,22 @@
 //
 
 import UIKit
+import AVFoundation
 
 class FirstViewController: UIViewController {
+    @IBAction func PausePlay(sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            backgroundMusicPlayer.play()
+        }
+        
+        else if sender.selectedSegmentIndex == 1 {
+            backgroundMusicPlayer.pause()
+        }
+        
+    }
 
+    var backgroundMusicPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,6 +35,9 @@ class FirstViewController: UIViewController {
         else {
             self.view.backgroundColor = UIColor.lightGrayColor()
         }
+        //playSound("BGMusic2")
+        //audioPlay.play()
+        playBackgroundMusic("BGMusic2.mp3")
 
     }
 
@@ -29,7 +45,22 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func playBackgroundMusic(filename: String) {
+        let url = NSBundle.mainBundle().URLForResource(filename, withExtension: nil)
+        guard let newURL = url else {
+            print("Could not find file: \(filename)")
+            return
+        }
+        do {
+            backgroundMusicPlayer = try AVAudioPlayer(contentsOfURL: newURL)
+            backgroundMusicPlayer.numberOfLoops = -1
+            backgroundMusicPlayer.prepareToPlay()
+            backgroundMusicPlayer.play()
+        } catch let error as NSError {
+            print(error.description)
+        }
+    }
+    
 }
 
